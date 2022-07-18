06c0939fd33b65a4110cd8cae2af4c5ad6abb93e
Krishna Pratap Singh <kpsingh1234@gmail.com>
2022-07-18 16:09:35 +0530

    Update DemoSingleton.java

--- src/com/java/design/patterns/creational/singleton/DemoSingleton.java
+++ src/com/java/design/patterns/creational/singleton/DemoSingleton.java
@@ -52,8 +52,27 @@ public class DemoSingleton implements Serializable {
 	 * This method will be invoked when you will de-serialize the object. Inside of
 	 * this method, you must return the existing instance to ensure a single
 	 * instance application wide.
+	 * 
+	 * 
+	 * For Serializable and Externalizable classes, the readResolve() method allows
+	 * a class to replace/resolve the object read from the stream before it is
+	 * returned to the caller. By implementing the readResolve method, a class can
+	 * directly control the types and instances of its own instances being
+	 * deserialized. The method is defined as follows:
+	 * 
+	 * ANY-ACCESS-MODIFIER Object readResolve() throws ObjectStreamException;
+	 * 
+	 * The readResolve method is called when ObjectInputStream has read an object
+	 * from the stream and is preparing to return it to the caller.
+	 * 
+	 * ObjectInputStream checks whether the class of the object defines the
+	 * readResolve method. If the method is defined, the readResolve method is
+	 * called to allow the object in the stream to designate the object to be
+	 * returned. The object returned should be of a type that is compatible with all
+	 * uses. If it is not compatible, a ClassCastException will be thrown when the
+	 * type mismatch is discovered.
 	 */
-	
+
 	protected Object readResolve() {
 		return getInstance();
 	}
